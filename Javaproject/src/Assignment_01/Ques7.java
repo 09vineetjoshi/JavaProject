643b73fa43077eab83cd15a6d6f5bdc731ce924f
vineet <vineet@LAPTOP-VA6THN50>
2022-05-16 17:49:30 +0530

    github

---
+++ Javaproject/src/Assignment_01/Ques7.java
@@ -0,0 +1,36 @@
+package Assignment_01;
+
+public class Ques7 {
+
+	public static void main(String[] args) {
+		// count of object created 
+		// count of display() method called
+		Employee1 obj1 = new Employee1();
+		Employee1 obj2 = new Employee1();
+		Employee1 obj3 = new Employee1();
+		obj2.display();
+		obj3.display();
+		obj1.print();
+
+	}
+}
+
+class Employee1
+{
+	static int object_count = 0;
+	static int method_count = 0;
+	Employee1()
+	{
+		object_count ++;
+	}
+	
+	void display()
+	{
+		method_count++;
+	}
+	
+	void print()
+	{
+		System.out.println("Object count "+ object_count + "\nmethod count "+method_count);
+	}
+}
